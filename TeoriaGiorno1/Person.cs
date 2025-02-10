62e1df822b0df777f8ef98882da42d0640cabf2e
s.demeis <s.demeis@capoweb.it>
2025-02-10 16:34:10 +0100

    Aggiungere i file di progetto.

---
+++ TeoriaGiorno1/Person.cs
@@ -0,0 +1,50 @@
+﻿namespace TeoriaGiorno1
+{
+    public class Person
+    {
+        private string? _firstName;
+        private string? _lastName;
+        private int _age;
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                _age = value;
+            }
+        }
+
+        public void SayHi()
+        {
+            Console.WriteLine("Hello, my name is " + FirstName + " " + LastName + " and I'm " + Age + " years old." );
+        }
+    }
+}
---
+++ TeoriaGiorno1/Program.cs
@@ -0,0 +1,9 @@
+﻿using TeoriaGiorno1;
+
+var person1 = new Person();
+
+person1.FirstName = "Mario";
+person1.LastName = "Rossi";
+person1.Age = 50;
+
+person1.SayHi();
